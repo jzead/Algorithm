001f14e7d9f95a66d72e5892f9f9b9a5d4e3900f
jzead <65839446+jzead@users.noreply.github.com>
2024-06-07 14:21:59 +0900

    Add files via upload

---
+++ prime number.py
@@ -0,0 +1,12 @@
+def prime(n):
+    for i in range(2,int(n**(1/2))+1):
+        if n%i == 0:
+            return print("no prime number")
+        
+    return print("prime number")
+
+
+n = int(input())
+prime(n)
+
+    
